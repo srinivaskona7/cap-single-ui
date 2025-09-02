4eb95302f1e7054755429d34f5672abd842b47dd
Srinivas Kona <srinivas_kona@apple.com>
2025-09-02 22:28:51 +0530

    all files

--- app/html5-deployer/resources/project1/controller/View1-dbg.controller.js
+++ app/html5-deployer/resources/project1/controller/View1-dbg.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], (Controller, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/m/ColumnListItem",
+    "sap/m/Text"
+], (Controller, JSONModel, ColumnListItem, Text) => {
     "use strict";
 
     return Controller.extend("project1.controller.View1", {
@@ -14,6 +16,36 @@ sap.ui.define([
 
             oContext.requestObject().then((data) => {
                 userModel.setProperty("/email", data.name);
+            });
+
+            const oRouter = this.getOwnerComponent().getRouter();
+            oRouter.getRoute("RouteView1").attachPatternMatched(this.onRoutePatternMatched, this);
+        },
+
+        onRoutePatternMatched: function() {
+            const oTable = this.byId("booksTable");
+            const oMessageStrip = this.byId("authMessage");
+            const odataModel = this.getOwnerComponent().getModel();
+
+            const oAuthContext = odataModel.bindContext("/isAuthorized()");
+            oAuthContext.requestObject().then(() => {
+                // User is authorized
+                oTable.bindItems({
+                    path: "/Books",
+                    template: new ColumnListItem({
+                        cells: [
+                            new Text({ text: "{ID}" }),
+                            new Text({ text: "{title}" }),
+                            new Text({ text: "{stock}" })
+                        ]
+                    })
+                });
+            }).catch((oError) => {
+                if (oError && /403|forbidden/i.test(oError.message)) {
+                    // User is not authorized
+                    oTable.setVisible(false);
+                    oMessageStrip.setVisible(true);
+                }
             });
         }
     });
